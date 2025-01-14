915a3e06bdff006788ef494ab134bfa4c63c24cb
Dana <djerad025@gmail.com>
2025-01-14 18:06:41 +0100

    final tue 14jan 3.1 Rewrote code for all locations

--- SAVEALWAYS.py
+++ SAVEALWAYS.py
@@ -1,33 +1,43 @@
 import json
 import csv
-#intercity rains 
+
+#this is for part 3 
+with open('ucaccmet2j_python/stations.csv') as file:
+    stations_list = list(csv.DictReader(file))
 
 with open('ucaccmet2j_python/precipitation.json', 'r', encoding='utf-8') as file:
     content = json.load(file)
 
-Seattle = {}
 
 months = '01 02 03 04 05 06 07 08 09 10 11 12'.split()
-pre_permonth = {}
 
-relative= {}
-total = 0 
 
-for month in months: 
-    pre_permonth[month] = 0 
-    relative[month] = 0 
+#I need to get the for city in cities:
+cities = {}
+for station in stations_list:
+    city = {}
+    pre_permonth = {}
 
-for item in content:
-    if item ['station'] == "GHCND:US1WAKG0038":
+    relative= {}
+
+    for month in months: 
+        pre_permonth[month] = 0 
+        relative[month] = 0 
+    print(station["Station"])
+    total = 0 
+    for item in content:
+        if item ['station'] == station["Station"]:
             #total 
-        total += item["value"]
-        for month in months: 
-            if item['date'].startswith(f"2010-{month}"):
-                pre_permonth[month] += item['value']
-            relative[month] = pre_permonth[month] / total 
+            total += item["value"]
+            for month in months: 
+                if item['date'].startswith(f"2010-{month}"):
+                    pre_permonth[month] += item['value']
+        
+    for month in pre_permonth:
+        relative[month] = pre_permonth[month] / total 
 
 
-# alternative way for the total: make a list (this works) 
+# alternative way for the total: make a list (this works but it doesnt fit inside my for loop)
 # totalpre = []
 #for month in pre_permonth:
     #totalpre.append(pre_permonth[month])
@@ -35,13 +45,17 @@ for item in content:
 #total = sum(totalpre)
 
 
+    #here is my seattle dictionary which will print in the json file 
+    city["pre_permonth"] = pre_permonth
+    city["total_precipiation"] = total
+    city["relative_precipitations"] = relative
+    
+    cities[station["Location"]] = city
 
-print("remember to save!")
-
-
-Seattle["pre_permonth"] = pre_permonth
-Seattle["total_precipiation"] = total
-Seattle["relative_precipitations"] = relative
 
 with open('results.json', 'w') as file:
-    json.dump(Seattle, file, indent=4)
+    json.dump(cities, file, indent=4)
+
+
+#i really need this to remember to save, so this is very important
+print("remember to save!")
